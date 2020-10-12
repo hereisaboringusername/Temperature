190b72af3bd91d3de058e3ab72d8ae89fdce9c60
hereisaboringusername <71794563+hereisaboringusername@users.noreply.github.com>
2020-10-12 12:32:16 -0400

    Add files via upload

--- conditions.py
+++ conditions.py
@@ -1,4 +1,7 @@
-#conditions.py
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #conditions.py
 temp = input("Please enter the current temperature: ")
 
 if temperature >=90:
